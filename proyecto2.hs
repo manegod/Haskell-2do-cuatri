e0729e5a772a4333b099c7dc4570d98eb0d7db0a
manegod <128608074+manegod@users.noreply.github.com>
2023-09-17 19:40:38 -0300

    Add files via upload

--- proyecto2.hs
+++ proyecto2.hs
@@ -128,5 +128,60 @@ encolar x (Encolada v y) = Encolada v (encolar x y)
 busca :: Cola -> Zona -> Maybe Deportista
 busca VaciaC _ = Nothing
 busca (Encolada x y) z = case x of
-                        Futbolista z _ _ _ -> Just x
-                        _ -> busca y z
+                        Futbolista z' _ _ _  -> if z==z' then Just x else busca y z
+
+
+
+data ListaAsoc a b = Vacia | Nodo a b (ListaAsoc a b) deriving (Show)
+
+--Nodo a b (ListaAsoc a b)
+--          ^^^^^^^^^^^^^
+--      Representa el resto de la lista, permite recorrer recursivamente los datos. 
+--      Represento esto con la palabra 'lista' en la linea 152. 
+
+
+type Diccionario = ListaAsoc String String
+
+type Padron = ListaAsoc Int String
+
+--Puedo hacerlo de la misma forma que esta definido Diccionario. El primer String 
+--puede representar un nombre y el segundo un numero telefonico.
+
+la_long :: ListaAsoc a b -> Int
+la_long Vacia = 0
+la_long (Nodo a b lista) = 1 + la_long lista
+
+--la_long (Nodo (Ajedrecista) 1 (Nodo (Ajedrecista) 3 Vacia)) = 2
+
+-- 'lista' es la parte de la lista que aún no se ha procesado y se 
+-- utiliza para realizar la recursión en esta función.
+
+la_concat :: ListaAsoc a b -> ListaAsoc a b -> ListaAsoc a b
+la_concat Vacia x = x
+la_concat x Vacia = x
+la_concat (Nodo a b lista) (Nodo x y lista') = Nodo a b (la_concat lista(Nodo x y lista'))
+
+ejem1 = Nodo "clave1" "valor1" (Nodo "clave2" "valor2" Vacia)
+ejem2= Nodo "clave3" "valor3" Vacia
+
+--la_concat ejem1 ejem2
+--Nodo "clave1" "valor1" (Nodo "clave2" "valor2" (Nodo "clave3" "valor3" Vacia))
+
+la_agregar :: Eq a => ListaAsoc a b -> a -> b -> ListaAsoc a b
+la_agregar Vacia a b = (Nodo a b Vacia)
+la_agregar (Nodo a b lista) x y | x==a = (Nodo a y lista)
+                                | x/=a = (Nodo a b (la_agregar lista x y))
+
+la_pares :: ListaAsoc a b -> [(a,b)]
+la_pares Vacia = []
+la_pares (Nodo a b lista) = [(a, b)]
+
+la_busca :: Eq a => ListaAsoc a b -> a -> Maybe b
+la_busca Vacia a = Nothing
+la_busca (Nodo x y lista) a | x==a = Just (y)
+                            | x/=a = la_busca lista a
+
+la_borrar :: Eq a => a -> ListaAsoc a b -> ListaAsoc a b
+la_borrar a Vacia = Vacia
+la_borrar a (Nodo x y lista) | a==x = la_borrar a lista
+                             | a/=x = (Nodo x y lista)
